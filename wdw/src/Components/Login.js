6f6c64ceea18894050e448c4bcfe0fe532b351e5
Adam <adamdlsoelter@gmail.com>
2021-02-01 16:35:36 -0600

    added a refresh token mechanism to make sure google login works continuously

--- wdw/src/Components/Login.js
+++ wdw/src/Components/Login.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {GoogleLogin} from 'react-google-login';
+import {RefreshTokenSetup} from '../util/RefreshTokenSetup';
 
 const clientId = '395610592492-f0jmbsmpkpi6616mn5vq31i1u9eib5ne.apps.googleusercontent.com';
 
 function Login() {
     const onSuccess = (res) => {
         console.log('[Login Success] currerntUser: ', res.profileObj);
+        RefreshTokenSetup(res);
     };
 
     const onFailure = (res) => {
