e3d768741173c3b6da8067679b9f72fbed8193b0
Alfonso Martello <alfonsomartello5@gmail.com>
2021-02-08 12:59:49 -0600

    Started reworking browser router

---
+++ wdw/src/Components/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+
+import NavBar from './Navbar';
+
+export const Login = () => {
+    return(
+        <div>
+            <Navbar />
+            <h1>Stuff</h1>
+        </div>
+    )
+};
