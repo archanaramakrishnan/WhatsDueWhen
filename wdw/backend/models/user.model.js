3485743967ef75c31fea1f05a580284a9f0d31eb
archanaramakrishnan <archana2359@gmail.com>
2021-02-10 14:11:04 -0600

    set up server and DB

---
+++ wdw/backend/models/user.model.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 3
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    birthdate: {
+        type: Date,
+        required: true,
+        unique: false
+    }
+  }, {
+    timestamps: true,
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
