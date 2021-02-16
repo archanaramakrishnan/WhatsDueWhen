2c7e8f57c8d976260dab6b4df3c9b6f7faaaccab
gkschnettgoecke <grant.schnettgoecke@gmail.com>
2021-02-16 02:41:16 -0600

    Updated information about a new user

--- wdw/backend/routes/users.js
+++ wdw/backend/routes/users.js
@@ -8,11 +8,14 @@ router.route('/').get((req, res) => {
 });
 
 router.route('/add').post((req, res) => {
-  const username = req.body.username;
-  const email = req.body.email;
-  const password = req.body.password;
-  const newUser = new User({username: username, email: email, password: password});
+  // const username = req.body.username;
+  // const email = req.body.email;
+  // const password = req.body.password;
+  // const newUser = new User({username: username, email: email, password: password});
   
+  const body = req.body;
+  const newUser = new User(body)
+
   newUser.save()
     .then(() => res.json('User added!'))
     .catch(err => res.status(400).json('Error: ' + err));
