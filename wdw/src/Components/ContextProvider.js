db4e526fe0abaa26b6d68b9676c1d63e2f3b2edc
Alfonso Martello <alfonsomartello5@gmail.com>
2021-03-15 12:21:33 -0500

    Removed isStudent function

--- wdw/src/Components/ContextProvider.js
+++ wdw/src/Components/ContextProvider.js
@@ -3,8 +3,6 @@ import React, {useState, useEffect} from "react";
 export const Context = React.createContext({
     userEmailContext: "",
     setUserEmailContext: () => {},
-    isStudent: false,
-    setIsStudent: () => {},
     isProfessor: false,
     setIsProfessor: () => {}
 });
