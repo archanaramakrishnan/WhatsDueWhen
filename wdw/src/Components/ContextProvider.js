37831f9fd23f0283fe38b0c1cc9901ff98af55e2
Alfonso Martello <alfonsomartello5@gmail.com>
2021-03-15 12:15:12 -0500

    Forgot to push context file

---
+++ wdw/src/Components/ContextProvider.js
@@ -0,0 +1,34 @@
+import React, {useState, useEffect} from "react";
+
+export const Context = React.createContext({
+    userEmailContext: "",
+    setUserEmailContext: () => {},
+    isStudent: false,
+    setIsStudent: () => {},
+    isProfessor: false,
+    setIsProfessor: () => {}
+});
+
+export const ContextProvider = (props) => {
+    const [userEmailContext, setUserEmailContext] = useState("");
+    const [isProfessor, setIsProfessor] = useState(false);
+
+    useEffect(() => {
+        console.log(`The user email is ${userEmailContext}`);
+    }, [userEmailContext])
+
+    return(
+        <Context.Provider
+            value={{
+                userEmailContext,
+                setUserEmailContext,
+                isProfessor,
+                setIsProfessor
+            }}
+        >
+            {props.children}
+        </Context.Provider>
+    );
+};
+
+export default ContextProvider;
