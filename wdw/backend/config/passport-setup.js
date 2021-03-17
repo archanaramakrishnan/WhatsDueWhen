0dde3c978464f56992fd9a023107fa8db86b7c3b
Adam <adamdlsoelter@gmail.com>
2021-03-17 14:22:29 -0500

    finished integrating google login with the google login button

--- wdw/backend/config/passport-setup.js
+++ wdw/backend/config/passport-setup.js
@@ -19,37 +19,23 @@ passport.use(
         clientID: '567371174244-fv4depvnt5kli81tk6msun52hpsvvce5.apps.googleusercontent.com',
         clientSecret: 'ALuB6DdhGBHEsExFc_ElZaXP'
     }, (accessToken, refreshToken, profile, done) => {
-        // passport callback function
-        console.log('passport callback function fired:');
-        // console.log(profile.emails[0].value);
-        // const googleEmail = profile.emails[0].value
-
-        User.findOne({googleId: profile.id}, (err, foundUser) => {
-            if (err) {
-                return done(err, foundUser)
-            }
-
-            if (foundUser) {
-                // already have this user
-                console.log('user is: ', foundUser);
-                done(null, foundUser);
-            } else {
-                const newUser = new User({
-                    firstname: profile.name.givenName,
-                    lastname: profile.name.familyName,
-                    googleId: profile.id,
-                    email: profile.emails[0].value,
-                    isProfessor: true
-                })
-
-                newUser.save().then(() => {
-                    console.log('created new user: ', newUser)
-                    done(null, newUser)
-                })
-
-            }
-            console.log('done')
-        })
-
+       User.findOne({googleId: profile.id}).then((foundUser) => {
+        if(foundUser)
+        {
+            done(null, foundUser);
+        }
+        else
+        {
+            new User({
+                firstname: profile.name.givenName,
+                lastname: profile.name.familyName,
+                googleId: profile.id,
+                email: profile.emails[0].value,
+                isProfessor: true
+            }).save().then((newUser) => {
+                done(null, newUser);
+            });
+        }
+       })
     })
 )
--- wdw/backend/routes/auth-routes.js
+++ wdw/backend/routes/auth-routes.js
@@ -15,17 +15,12 @@ router.get('/logout', (req, res) => {
 
 // auth with google
 router.get('/google', passport.authenticate('google', {
-    //scope: ['profile', 'email']
-    scope: ['profile']
+    scope: ['profile', 'email']
 }));
 
 // callback route for google to redirect to
 router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
-    //res.send('you reached the redirect URI. The user will be redirected back to the front end EXTRA STUFF.');
-    //res.send('second message')
-    res.send(req.user)
-    res.redirect('http://localhost:3000')
-    
+    res.redirect('http://localhost:3000/calendarpage')
 });
 
 module.exports = router;
