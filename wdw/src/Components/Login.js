a5b5b4aeb320061ee807b4f38a719f6a138d88c9
Alfonso Martello <alfonsomartello5@gmail.com>
2021-03-19 14:01:11 -0500

    Started working on adding user to database

--- wdw/src/Components/Login.js
+++ wdw/src/Components/Login.js
@@ -41,8 +41,6 @@ export const Login = () => {
     const [createAccountAsStudent, setCreateAccountAsStudent] = useState(true);
 
     useEffect(() => {
-        //initialize a new user as a student to handle default case where they don't click a button
-        setIsStudent(true);
         axios.get('http://localhost:5000/users/')
         .then(res => console.log(res.data));
     }, [])
