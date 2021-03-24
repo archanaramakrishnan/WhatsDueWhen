65b369dcbf8888e5b6c0a354fe85ed4d31cb9efc
Alfonso Martello <alfonsomartello5@gmail.com>
2021-03-24 13:28:35 -0500

    Redirect non-logged in users back to the login-page

--- wdw/src/Components/Home.js
+++ wdw/src/Components/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Home.css'
 import CheckboxList from "./CheckboxList"
 import CurrentDate from "./CurrentDate"
@@ -8,6 +8,14 @@ import {Scheduler,MonthView,DateNavigator,
 import { ViewState } from '@devexpress/dx-react-scheduler';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+
+import { useHistory } from 'react-router-dom';
+
+//Providing global access to logged-in user email
+import { Context } from './ContextProvider';
+
+//For contecting to our backend
+import axios from 'axios';
 
 let today = new Date();
 let dd = String(today.getDate()).padStart(2, '0');
@@ -22,30 +30,37 @@ let appointments = [
 ];
 
 export const Home = () => {
+  //Get value of logged in user's email and their status as a professor or student
+  const {userEmailContext, isProfessor} = React.useContext(Context);
+
+  //Allow us to navigate to other components based on URL
+  const history = useHistory();
 
   const [data, setData] = useState(appointments);
   const [currentDate, setCurrentDate] = useState(today);
 
+  useEffect(() => {
+    //if user is not logged-in, redirect them to the login page
+    if (userEmailContext == "")
+    {
+      history.push("/");
+    }
+  }, [])
+
   return (
     <div>
-      {/* <Paper className='welcome'>
-        <p className='date-title'> On Your Calendar Today</p>
-        <CurrentDate></CurrentDate>
-        <p>.{"\n"}</p>
-      </Paper> */}
       <Paper style={{float: "left", width: "48.5%"}}>
         <div className="title">
-      <Typography variant="h3" gutterBottom>
-          On Your Calendar Today
-       </Typography>
-        <Typography variant="h5" gutterBottom>
-        <CurrentDate />
-       </Typography>
-       </div>
-      <CheckboxList />
+          <Typography variant="h3" gutterBottom>
+            On Your Calendar Today
+          </Typography>
+          <Typography variant="h5" gutterBottom>
+            <CurrentDate />
+          </Typography>
+        </div>
+        <CheckboxList />
       </Paper>
       <Paper style={{width: "50%", float: "left", marginLeft: "15px"}}>
-      
         <Scheduler
           height={600}
         >
