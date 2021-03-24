fe486638546f0d2ed05c7c783168e2338681193b
Adam <adamdlsoelter@gmail.com>
2021-03-24 14:02:32 -0500

    changed google login redirect route to calendarpage while home is not working

--- wdw/backend/routes/auth-routes.js
+++ wdw/backend/routes/auth-routes.js
@@ -20,7 +20,7 @@ router.get('/google', passport.authenticate('google', {
 
 // callback route for google to redirect to
 router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
-    res.redirect('http://localhost:3000/home')
+    res.redirect('http://localhost:3000/calendarpage')
 });
 
 module.exports = router;
