d6ec8fdac79d24ee5e3f0dac5a929a79224254bc
archanaramakrishnan <archana2359@gmail.com>
2021-04-24 14:12:52 -0500

    copy permission number to clipboard

--- wdw/src/Components/SubjectSelector.js
+++ wdw/src/Components/SubjectSelector.js
@@ -2,26 +2,51 @@ import React, { useState } from 'react';
 import Switch from '@material-ui/core/Switch';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+import ClipboardIcon from 'react-clipboard-icon'
 
 function SubjectSelector(props) {
 
     const [switchSel, setSwitchSel] = useState(true);
+    const [copied, setCopied] = useState(false);
 
     const handleChange = () => {
         setSwitchSel(!switchSel)
     }
+
+    const onCopy = () => {
+        setCopied(true);
+      };
+
+    const onClick = ({target: {innerHTML}}) => {
+        console.log(`Clicked on "${innerHTML}"!`); // eslint-disable-line
+      };
 
     return (
         <Card style={{ height: "120px" }}>
             <div style={{ padding: "25px", display: "flex", justifyContent: "space-between" }}>
                 <div style={{ float: "left" }}>
                     {props.name}
-                    <div style={{ fontSize: "9pt" }}>
-                        {"Permission Number : " + props.permNum}
-                    </div>
-                    <Button variant="outlined" size="small">
+                    {<CopyToClipboard
+                        onCopy={onCopy}
+                        text={props.permNum}>
+                        <Button onClick={onClick} variant="outlined" size="small">
+                            <div style={{ fontSize: "9pt" }}>
+                                {"Permission Number : " + props.permNum } 
+                                &nbsp;
+                            </div>
+                            
+                            <div>
+                                <ClipboardIcon
+                                size={20}
+                                style={{fill: '#1a237e' }}
+                                />
+                            </div>
+                        </Button> 
+                    </CopyToClipboard> }
+                    {/* <Button variant="outlined" size="small">
                         Add Event
-                </Button>
+                    </Button> */}
                 </div>
                 <div style={{ float: "left" }}>
                     <Switch checked={switchSel} color={"primary"} label={props.name} onChange={handleChange} />
