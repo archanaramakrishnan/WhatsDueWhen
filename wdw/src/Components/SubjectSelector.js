f27a99736ef982237fa05118a64c5c80a2151985
gkschnettgoecke <grant.schnettgoecke@gmail.com>
2021-04-24 16:20:16 -0500

    subjectselector color box

--- wdw/src/Components/SubjectSelector.js
+++ wdw/src/Components/SubjectSelector.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Switch from '@material-ui/core/Switch';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
+import Box from '@material-ui/core/Box';
 
 function SubjectSelector(props) {
 
@@ -14,14 +15,16 @@ function SubjectSelector(props) {
     return (
         <Card style={{ height: "120px" }}>
             <div style={{ padding: "25px", display: "flex", justifyContent: "space-between" }}>
+                <Box bgcolor={props.color} p={2} />
+                
                 <div style={{ float: "left" }}>
                     {props.name}
                     <div style={{ fontSize: "9pt" }}>
                         {"Permission Number : " + props.permNum}
                     </div>
-                    <Button variant="outlined" size="small">
+                    {/* <Button variant="outlined" size="small">
                         Add Event
-                </Button>
+                    </Button> */}
                 </div>
                 <div style={{ float: "left" }}>
                     <Switch checked={switchSel} color={"primary"} label={props.name} onChange={handleChange} />
